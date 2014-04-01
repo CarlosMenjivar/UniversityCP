45a9882d747063ca6a0349fd921468a3449c4ea6
Benito Palacios <benito356@gmail.com>
2014-04-02 01:57:35 +0200

    P4: Implementada clase Ruta

--- P4/TSP/src/tsp/Ruta.java
+++ P4/TSP/src/tsp/Ruta.java
@@ -17,12 +17,103 @@
 
 package tsp;
 
+import java.util.Arrays;
+
 /**
- *
+ * Clase que representa una ruta desde una ciudad origen y que pasa
+ * por todas las ciudades existentes hasta volver a la de origen.
+ * 
+ * Representa una solución al problema TSP.
  * 
  * @version 1.0
  * @author  Benito Palacios Sánchez
  */
 public class Ruta {
+    /**
+     * Ciudades en orden ascendente que componen la ruta.
+     */
+    private final Ciudad[] ruta;
+    
+    /**
+     * Índice de la primera ciudad que contiene la variable ruta.
+     * Una vez finalizado el algoritmo este valor ha de ser 0.
+     * Permite dar soluciones parciales y temporales al problema.
+     */
+    private int primeraCiudad;
+    
+    /**
+     * Índice de la última ciudad que contiene la variable ruta.
+     * Una vez finalizado el algoritmo este valor ha de ser ruta.length - 1.
+     * Permite dar soluciones parciales y temporales al problema.
+     */
+    private int ultimaCiudad;
+    
+    /**
+     * Crea una instancia reservando el número de ciudades especificado.
+     * 
+     * @param numCiudades Número de ciudades que contendrá la ruta.
+     */
+    public Ruta(int numCiudades) {
+        this.ruta = new Ciudad[numCiudades];
+        this.primeraCiudad = -1;
+        this.ultimaCiudad  = -1;
+    }
+    
+    /**
+     * Devuelve el conjunto de ciudades que actualmente definen a la ruta.
+     * Se devolverá una subarray con solo las ciudades insertadas.
+     * 
+     * @return Conjunto de ciudades que definen la ruta.
+     */
+    public Ciudad[] getRuta() {
+        if (this.primeraCiudad == -1 || this.ultimaCiudad == -1)
+            return null;
+          
+        Ciudad[] subruta = Arrays.copyOfRange(
+                this.ruta,
+                this.primeraCiudad,
+                this.ultimaCiudad + 1
+        );
+        
+        return subruta;
+    }
+    
+    /**
+     * Devuelve el coste total de la ruta actual.
+     * 
+     * @return Coste de la ruta actual.
+     */
+    public double getCoste() {
+        if (this.primeraCiudad == -1 || this.ultimaCiudad == -1)
+            return -1;
+        
+        double coste = 0;
+        
+        for (int i = this.primeraCiudad; i < this.ultimaCiudad; i++)
+            coste += this.ruta[i].getDistancia(this.ruta[i + 1]);
+        
+        return coste;
+    }
+    
+    /**
+     * Inserta una ciudad en la posición dada.
+     * 
+     * @param ciudad Ciudad a insertar.
+     * @param pos Posición de la ruta donde se insertará la ciudad.
+     */
+    public void insertCiudad(Ciudad ciudad, int pos) {
+        if (pos < 0 || pos > this.ruta.length)
+            throw new ArrayIndexOutOfBoundsException(pos);
+        
+        if (this.primeraCiudad == -1 || this.primeraCiudad > pos)
+            this.primeraCiudad = pos;
+        
+        if (this.ultimaCiudad == -1 || this.ultimaCiudad < pos)
+            this.ultimaCiudad = pos;
+        
+        // Dado que el objeto ciudad es inmutable (hay definidos setters)
+        // no hace falta clonarlo.
+        this.ruta[pos] = ciudad;
+    }
     
 }
