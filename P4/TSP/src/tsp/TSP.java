9187e5392025e2259f8f941609a9acbbbfc873b9
Benito Palacios <benito356@gmail.com>
2014-04-02 15:48:36 +0200

    P4: Implementado método principal

--- P4/TSP/src/tsp/TSP.java
+++ P4/TSP/src/tsp/TSP.java
@@ -17,6 +17,9 @@
 
 package tsp;
 
+import org.omg.IOP.CodecPackage.FormatMismatch;
+import tsp.algoritmos.*;
+
 /**
  * Clase principal del programa donde se lee los argumentos y se ejecutan
  * los algoritmos.
@@ -25,14 +28,139 @@ package tsp;
  * @author  Benito Palacios Sánchez
  */
 public class TSP {
-
+    /** Conjunto de algoritmos para resolver el problema soportados. */
+    private static final ITspAlgoritmo[] Algoritmos = new ITspAlgoritmo[] {
+        
+    };
+    
     /**
      * Método de entrada del programa.
      * 
      * @param args Los argumentos de la consola.
      */
     public static void main(final String[] args) {
-        // TODO:
+        if (args.length == 0 || args[0].equals("help")) {
+            muestraAyuda();
+            return;
+        }
+
+        // Opciones por defecto
+        boolean muestraSolucion = false;
+        boolean muestraRuta     = false;
+        boolean muestraCoste    = false;
+        ITspAlgoritmo algoritmo = Algoritmos[0];
+
+        // Lee los argumentos del programa.
+        for (int i = 0; i < args.length; i++) {
+            switch (args[i]) {
+                case "solucion": muestraSolucion = true; break;
+                case "ruta":     muestraRuta     = true; break;
+                case "coste":    muestraCoste    = true; break;
+                case "algo":
+                    if (i + 1 >= args.length) {
+                        muestraAyuda();
+                        return;
+                    }
+
+                    int numAlgo = Integer.parseInt(args[i + 1]);
+                    if (numAlgo >= Algoritmos.length) {
+                        muestraAyuda();
+                        return;
+                    }
+
+                    algoritmo = Algoritmos[numAlgo];
+                    break;
+            }
+        }
+
+        // Lee el problema
+        Problema problema;
+        try {
+            problema = Problema.FromStdIn();
+        } catch (FormatMismatch ex) {
+            System.out.println(ex.getMessage());
+            return;
+        }  
+        
+        // Resuelve el problema
+        Heuristica heuristica = new Heuristica(problema, algoritmo);
+        heuristica.run();
+        
+        // Muestra el resultado
+        Ruta ruta = heuristica.getRuta();
+        if (muestraCoste)
+            muestraCoste(ruta);
+        
+        if (muestraRuta)
+            muestraRuta(ruta);
+        
+        if (muestraSolucion)
+            muestraSolucion(ruta);
     }
     
+    /**
+     * Muestra los parámetros y el funcionamiento del programa.
+     */
+    private static void muestraAyuda() {
+        System.out.println("USO: TSP [h] [solucion] [ruta] [coste] [algo X]");
+        System.out.println();
+        System.out.println("El problema se introducirá desde la entrada    ");
+        System.out.println("estandar.                                      ");
+        System.out.println();
+        System.out.println("Opciones:");
+        System.out.println("   h:        Muestra esta ayuda.               ");
+        System.out.println("   solucion: Muestra las coordenadas de cada   ");
+        System.out.println("              ciudad en orden según la ruta.   ");
+        System.out.println("   ruta:     Muestra el ID de cada ciudad en   ");
+        System.out.println("              orden según la ruta.             ");
+        System.out.println("   coste:    Muestra el coste de la ruta.      ");
+        System.out.println("   algo X:   Utiliza el número de algoritmo    ");
+        System.out.println("              seleccionado.                    ");
+        System.out.println("             0: VecinoMasCercano [por defecto] ");
+        System.out.println("             1: InsercionEconomica             ");
+        System.out.println("             2: InsercionLejana                ");
+        System.out.println("             3: RutaAleatoria                  ");
+        System.out.println("             4: MejoraAleatoria                ");
+    }
+        
+    /**
+     * Realiza una comparativa de cada algoritmo implementado comprobando
+     * el coste de cada solución y el tiempo que tarda en encontrarla.
+     * 
+     * @param problema Problema a resolver.
+     */
+    private static void comparaAlgoritmos(Problema problema) {
+        throw new UnsupportedOperationException("Not implemented yet!");
+    }
+    
+    /**
+     * Muestra el coste de la ruta.
+     * 
+     * @param solucion Ruta encontrada.
+     */
+    private static void muestraCoste(Ruta solucion) {
+        System.out.println("MEJOR SOLUCIÓN: " + solucion.getCoste());
+    }
+    
+    /**
+     * Muestra las coordenadas de cada ciudad en el orden en el que se
+     * visitarán.
+     * 
+     * @param solucion Ruta encontrada.
+     */
+    private static void muestraSolucion(Ruta solucion) {
+        for (Ciudad ciudad : solucion.getRuta())
+            System.out.println(ciudad.getCoordX() + " " + ciudad.getCoordY());
+    }
+    
+    /**
+     * Muestra los identificadores de cada ciudad en el orden en el que se
+     * visitarán.
+     * 
+     * @param solucion Ruta encontrada.
+     */
+    private static void muestraRuta(Ruta solucion) {
+        for (Ciudad ciudad : solucion.getRuta())
+            System.out.println(ciudad.getId());
+    }
 }
