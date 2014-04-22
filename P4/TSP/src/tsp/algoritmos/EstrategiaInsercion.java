295308af3a35026f07e54a3cd79e5ce522e3c6aa
Benito Palacios Sánchez <benito356@gmail.com>
2014-04-22 21:59:36 +0200

    P4: Implementada base para estrategias de inserción

---
+++ P4/TSP/src/tsp/algoritmos/EstrategiaInsercion.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright (C) 2014 Benito Palacios Sánchez
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package tsp.algoritmos;
+
+import java.util.Arrays;
+import java.util.List;
+import tsp.Ciudad;
+import tsp.Problema;
+import tsp.Ruta;
+
+/**
+ * Algoritmo mejorado con diferentes estrategias de inserción.
+ * Resuleve el problema TSP.
+ * 
+ * @version 1.0
+ * @author Benito Palacios Sánchez
+ */
+public class EstrategiaInsercion implements ITspAlgoritmo {
+    private final IInsercion insercion;
+    
+    /**
+     * Crea una nueva instancia de este algoritmo con el algoritmo de inserción
+     * dado.
+     * 
+     * @param insercion Algoritmo de inserción a usar.
+     */
+    public EstrategiaInsercion(final IInsercion insercion) {
+        this.insercion = insercion;
+    }
+    
+    @Override
+    public Ruta calculaMejorRuta(final Problema problema) {
+        // Ciudades sin visitar, para los algoritmos de inserción
+        List<Ciudad> sinVisitar = Arrays.asList(problema.getCiudades());
+        
+        // Genero la ruta inicial, y elimino esas ciudades de "sinVisitar".
+        Ruta ruta = this.calculaRutaInicial(problema);
+        sinVisitar.removeAll(Arrays.asList(ruta.getRuta()));
+        
+        // Mientras haya ciudades sin visitar, llamo al algoritmo de inserción
+        while (sinVisitar.size() > 0)
+            this.insercion.insertaSiguiente(sinVisitar, ruta);
+        
+        return ruta;
+    }
+    
+    /**
+     * Genera el recorrido inicial del que se parte.
+     * Coge las tres ciudades que forman el triángulo más grande.
+     * 
+     * @param problema
+     * @return 
+     */
+    private Ruta calculaRutaInicial(final Problema problema) {
+        Ruta ruta = new Ruta(problema.getNumCiudades());
+        
+        // Ciudad más arriba
+        int yMax = -1;
+        for (Ciudad c : problema.getCiudades()) {
+            if (c.getCoordY() > yMax)
+                ruta.insertCiudad(c, 0);
+        }
+        
+        // Ciudad más a la izquierda
+        int xMin = Integer.MAX_VALUE;
+        for (Ciudad c : problema.getCiudades()) {
+            if (c.getCoordX() < xMin)
+                ruta.insertCiudad(c, 1);
+        }
+        
+        // Ciudad más a la derecha
+        int xMax = -1;
+        for (Ciudad c : problema.getCiudades()) {
+            if (c.getCoordX() > xMax)
+                ruta.insertCiudad(c, 2);
+        }
+        
+        return ruta;
+    }
+}
