2b4d0b33a362b993a04308b6b98ac6fa60bdebe4
Benito Palacios Sánchez <benito356@gmail.com>
2014-05-28 10:26:02 +0200

    P7: Solucionado fallo de rotación inversa

--- P7/Tetris/src/data/Figura.java
+++ P7/Tetris/src/data/Figura.java
@@ -49,9 +49,9 @@ public class Figura {
         this.figura = new Bloque[SIZE_Y][SIZE_X];
         
         // Por cada fila
-        for (int y = 0; y < figura.length; y++) {
+        for (int y = 0; y < SIZE_Y; y++) {
             // Por cada columna de la fila
-            for (int x = 0; x < figura.length; x++) {
+            for (int x = 0; x < SIZE_X; x++) {
                 if (y >= filas.length) {
                     this.figura[y][x] = new Bloque(TipoBloque.VACIO, Color.white);
                 } else {
@@ -64,6 +64,14 @@ public class Figura {
         
         // Desplaza la figura para asegurarnos de que está en el borde inferior.
         this.shift();
+    }
+    
+    public static int getMaxSizeX() {
+        return SIZE_X;
+    }
+    
+    public static int getMaxSizeY() {
+        return SIZE_Y;
     }
     
     /**
@@ -134,21 +142,33 @@ public class Figura {
      */
     public void rotate(final Direccion direccion) {
         int a = 0;  // Como el ángulo es siempre 90º -> cos(90) = cos(-90) = 0;
-        int b = 1;  // sen(90º) = 1 -> giro en sentido contrario al reloj.
+        int b = 1;  // sen(-90º) = -1 -> giro en sentido del reloj.
         if (direccion == Direccion.ABAJO || direccion == Direccion.DERECHA)
-            b = -1; // sen(-90º) = -1 -> giro en el sentdio del reloj.
+            b = -1; // sen(90º) = 1 -> giro en el sentdio contrario al reloj.
         
         // Realiza la rotación para cada bloque de la figura y lo pone
         // en una nueva figura.
         Bloque[][] antigua = (Bloque[][])this.figura.clone();
-        this.figura = new Bloque[SIZE_Y][SIZE_X];
-        for (int x0 = 0; x0 < this.figura.length; x0++) {
-            for (int y0 = 0; y0 < this.figura[x0].length; y0++) {
+        int antSizeY = antigua.length;
+        int antSizeX = antigua[0].length;
+        this.figura = new Bloque[antSizeX][antSizeY];
+        for (int y0 = 0; y0 < antSizeY; y0++) {
+            for (int x0 = 0; x0 < antSizeX; x0++) {
                 Punto ant = new Punto(x0, y0);
                 Punto rot = this.rotate(ant, a, b);
                 int x1 = rot.getCoordX();
-                int y1 = rot.getCoordY() + (this.figura[x0].length - 1);
-                this.figura[x1][y1] = antigua[x0][y0];
+                int y1 = rot.getCoordY();
+                if (b == -1) {
+                    y1 += (antSizeX - 1);
+                    //x1 = (antSizeY - 1) - x1;
+                } else {
+                    x1 += (antSizeY - 1);
+                }
+                if (y1 >= SIZE_Y || y1 < 0)
+                    System.out.println("Error en Y: " + y1);
+                if (x1 >= SIZE_X || x1 < 0)
+                    System.out.println("Error en X: " + x1);
+                this.figura[y1][x1] = antigua[y0][x0];
             }
         }
         
@@ -203,11 +223,39 @@ public class Figura {
     }
     
     /**
-     * Desplaza una figura hacia el borde inferior.
+     * Obtiene el índice de la primera columna que contiene bloques de la figura.
+     * 
+     * @return Índice de la primera columna con bloques de la figura.
+     */    
+    private int getLeftPoint() {
+        int x = -1;
+        
+        // Comprueba cada columna hasta encontrar una que no sea nula
+        for (int c = 0; c < this.figura[0].length && x == -1; c++) {
+            // Comprueba cada bloque de la columna para ver si hay alguno lleno.
+            boolean columnaNula = true;
+            for (int f = 0; f < this.figura.length && columnaNula; f++) {
+                if (this.figura[f][c].getTipo() != TipoBloque.VACIO)
+                    columnaNula = false;
+            }
+            
+            // En caso de que no sea nula hemos terminado
+            if (!columnaNula)
+                x = c;
+        }
+        
+        return x;        
+    }
+    
+    /**
+     * Desplaza una figura hacia la esquina inferior izquierda.
      */
     private void shift() {
-        int posDesp = this.getBottonPoint();
-        this.shift(posDesp);
+        int posDown = this.getBottonPoint();
+        this.shiftDown(posDown);
+        
+        int posLeft = this.getLeftPoint();
+        this.shiftLeft(posLeft);
     }
     
     /**
@@ -215,7 +263,7 @@ public class Figura {
      * 
      * @param pos Posición desde la que empezará el movimiento.
      */
-    private void shift(int pos) {
+    private void shiftDown(int pos) {
         // Si no hay desde donde copiar o se va a copiar a sí mismo salir.
         if (pos == -1 || pos == this.figura.length - 1)
             return;
@@ -228,5 +276,27 @@ public class Figura {
         // Las restantes filas son nulas
         for ( ; f >= 0; f--)
             Arrays.fill(this.figura[f], new Bloque(TipoBloque.VACIO, Color.yellow));
+    }
+    
+    /**
+     * Desplaza la figura hacia la izquierda partiendo de una posición.
+     * 
+     * @param pos Posición desde la que empezará el movimiento.
+     */
+    private void shiftLeft(int pos) {
+        // Si no hay desde donde copiar o se va a copiar a sí mismo salir.
+        if (pos == -1 || pos == 0)
+            return;
+        
+        int c;
+        // Copio a partir de 'pos' hasta llegar al borde derecho.
+        for (c = 0; pos < this.figura[0].length; c++, pos++)
+            for (int f = 0; f < this.figura.length; f++)
+                this.figura[f][c] = this.figura[f][pos];
+        
+        // Las restantes filas son nulas
+        for ( ; c < this.figura[0].length; c++)
+            for (int f = 0; f < this.figura.length; f++)
+                this.figura[f][c] = new Bloque(TipoBloque.VACIO, Color.yellow);
     }
 }
